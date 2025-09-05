3e2af177eb11b27d9e6e7549b251b11d5ffac052
Yangluwei-coder <yanglw1@163.com>
2025-09-05 12:05:39 +0800

    update

--- tinijoy-frontend/src/features/products/Product.jsx
+++ tinijoy-frontend/src/features/products/Product.jsx
@@ -52,14 +52,63 @@ export default function Product() {
         <div className='product-box'>
           <div className='product-word'>
             <p style={{ marginTop: '20px', fontSize: '30px', fontWeight: '500' }}>{toy.name}</p>
-            <p style={{ marginTop: '30px', maxWidth: '400px', color: 'darkgrey', lineHeight: '1.5' }}>{toy.description}</p>
-            <p style={{ marginTop: '20px', fontSize: '22px', fontWeight: '500', color: 'rgb(162, 200, 36)' }}>${toy.price.toFixed(2)}</p>
+            <p style={{ marginTop: '30px', maxWidth: '400px', color: 'darkgrey', lineHeight: '1.5' }}>
+              A successful marketing plan relies heavily on the pulling-power of advertising copy.
+              Writing result-oriented ad copy is difficult, as it must appeal to, entice, and convince consumers to take action.
+              There is no magic formula to write perfect ad copy. It is based on a number of factors.
+            </p>
+            <p style={{ marginTop: '20px', fontSize: '22px', fontWeight: '500', color: 'rgb(162, 200, 36)' }}>{toy.price}</p>
             <div style={{ display: 'flex', gap: '10px' }}>
-              <input type="number" min="1" value={quantity} onChange={e => setQuantity(Number(e.target.value))} className="number-input"/>
+              <input type="number" min="1" value={quantity} onChange={(e) => setQuantity(e.target.value)}className="number-input"/>
               <button className="add-button" onClick={handleAddToCart}>Add to Cart</button>
             </div>
           </div>
           <div><img src={toy.image} alt={toy.name} style={{ width: '350px' }} /></div>
+        </div>
+
+        <div className='product-intro'>
+          <div className='detail-head'>
+            <div>Product Details</div>
+            <div style={{ fontSize: '14px' }}>{toy.name}</div>
+          </div>
+          <div className="slider-bar" style={{ margin: '20px auto', width: '100%' }}>
+            <div className="slider-progress" style={{ width: '20%' }}></div>
+          </div>
+          <div className='product-detail'>
+            <div className='left-detail'>
+              <p style={{ fontSize: '22px' }}>{toy.name} Description</p>
+              <div style={{ marginLeft: '20px', fontSize: '16px' }}>
+                <li>Colorful Design</li>
+                <li>Eco-friendly Materials</li>
+                <li>Safe for Children</li>
+                <li>Recommended Age: 3 years and up</li>
+              </div>
+              <p style={{ fontSize: '12px' }}>
+                Build with joy, spark creativity! A perfect choice for gifting and parent-child playtime!
+              </p>
+            </div>
+
+            <div className='right-form'>
+              <form className="styled-form">
+                <div className="form-line">
+                  <span className="label">Width</span>
+                  <span className="value">12.5 in</span>
+                </div>
+                <div className="form-line">
+                  <span className="label">Height</span>
+                  <span className="value">6 in</span>
+                </div>
+                <div className="form-line">
+                  <span className="label">Length</span>
+                  <span className="value">3 in</span>
+                </div>
+                <div className="form-line">
+                  <span className="label">Weight</span>
+                  <span className="value">32 oz</span>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
 
